a9e628be379b5965dc684664d43baa659a3dfd4e
ManMadeMachine <Eetu.Suonpaa@hotmail.com>
2016-06-02 23:19:06 +0300

    Started profile view implementation

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>Tähän tulee profiilin tietojen editointi</p>
-
-    <h3>Käyttäjä: {{ $user->name }}</h3>
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    {{ $user->name }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
