3ec1b86dd94c26eac591e524dab71344ff136a2d
ManMadeMachine <Eetu.Suonpaa@hotmail.com>
2016-06-03 19:02:28 +0300

    Edited profile edit view

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -3,10 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-2">
+        <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">
-                    {{ $user->name }}
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-3">
+                            Insert photo!
+                        </div>
+                        <div class="col-md-9">
+                            {{ $user->name }}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
